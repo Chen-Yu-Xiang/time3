7916035e99b3ce63b6178fd282ce2d7033dc5ff9
User19 <user19@user19sap.local>
2019-01-13 17:12:49 +0800

    Initial Commit

---
+++ time3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  time3
+//
+//  Created by User19 on 2019/1/13.
+//  Copyright © 2019 bob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
